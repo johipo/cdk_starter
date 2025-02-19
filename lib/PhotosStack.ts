cbd75b814621ba3e20890693fc4fff90625c48b9
Joao Hipolito <jvhipolito627@gmail.com>
2025-02-19 18:40:53 -0400

    Intrisic Functions

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -1,14 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
+import { Fn } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotosStack extends cdk.Stack {
+    private stackSuffix: string;
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
 
+      this.initializeSuffix();
+
       const myBucket = new Bucket(this, 'PhotosBucket2', {
-        bucketName: `photosbucket-002-${this.region}`
+        bucketName: `photos-bucket-${this.stackSuffix}-${this.region}`
       });
 
-      (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('ChildLogicalIDPhotosBucket002')
-}}
+      (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('ChildLogicalIDPhotosBucket002');
+    }
+
+    private initializeSuffix(){
+      const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
+      this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId))
+    }
+}
